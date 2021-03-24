799d7b98613604bf95160995604b85ad88c7dc36
Anatoly Korobov <a.korobov06@gmail.com>
2021-03-24 17:49:20 +0300

    test improve with author of publication in templates

--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -3,4 +3,4 @@ import pytest
 
 @pytest.fixture
 def user(django_user_model):
-    return django_user_model.objects.create_user(username='Test user', password='1234567')
+    return django_user_model.objects.create_user(username='Test user', password='1234567', first_name='test first name', last_name='test last name')
