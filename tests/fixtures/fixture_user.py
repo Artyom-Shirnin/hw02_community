a67043ae1881e482a3526c244a6c918500cc1bde
Anatoly Korobov <a.korobov06@gmail.com>
2021-03-26 10:17:28 +0300

    flake8

--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -3,4 +3,9 @@ import pytest
 
 @pytest.fixture
 def user(django_user_model):
-    return django_user_model.objects.create_user(username='Test user', password='1234567', first_name='test first name', last_name='test last name')
+    return django_user_model.objects.create_user(
+        username='Test user',
+        password='1234567',
+        first_name='test first name',
+        last_name='test last name'
+    )
