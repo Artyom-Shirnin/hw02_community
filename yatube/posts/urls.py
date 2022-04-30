2c90b7288fce60fcf69750e0e4d1adf4d9d82583
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-04-30 21:11:59 +1000

    Финал проекта

---
+++ yatube/posts/urls.py
@@ -0,0 +1,10 @@
+# posts/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'index'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+] 
---
+++ yatube/posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
